5cf1e847b8c906da983b5d4e31ae7f756084efdd
Madalin Popa <contact@madalinpopa.com>
2023-08-28 21:16:15 +0300

    Update database settings to use Postgresql

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "bookmarks.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bookmarks",
+        "USER": "bookmarks",
+        "PASSWORD": "demo123",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
