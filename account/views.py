3d1a18c71c1dcf95de92a84f5789db25d26ebac9
Madalin Popa <contact@madalinpopa.com>
2023-08-30 06:00:22 +0000

    Format code style

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+
 from .forms import LoginForm
 
 
